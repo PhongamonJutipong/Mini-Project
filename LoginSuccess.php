2534e1ba17fe9a907da398433ae6c9aa894a7b6e
pongpak726 <fuza2560@gmail.com>
2025-10-09 22:37:40 +0700

    mod login + LoginSuccess ให้ลองรับ password_hash

--- LoginSuccess.php
+++ LoginSuccess.php
@@ -8,37 +8,42 @@
 <body>
 
 <?php
+    session_start();
     require 'conn.php';
 
     if (isset($_POST['login'])) {
 
-        $username = $_POST['username'];
+        $username = trim($_POST['username']);
         $password = $_POST['password'];
         $role = $_POST['role'];
 
-        if ($role == 'admin') {
-            $sql = "SELECT * FROM users 
-            WHERE email='$username' 
-            AND password_hash='$password' 
-            AND role='admin'";
-        } 
-        else {
-            $sql = "SELECT * FROM users 
-            WHERE email='$username' 
-            AND password_hash='$password' 
-            AND role='user'";
-        }   
+        //chatgpt เพิ่มความปลอดภัย(sql injection)
+        // code นี้จะใช้ได้ก็ต่อเมื่อ password เป็น password_hash เท่านั้น
 
+        $stmt = $conn->prepare("SELECT * FROM users WHERE email=? AND role=? AND status='active'");
+        $stmt->bind_param("ss", $username, $role);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $user = $result->fetch_assoc();
 
+        if($user && password_verify($password, $user['password_hash'])){
+        $_SESSION['user_id'] = $user['user_id'];
+        $_SESSION['username'] = $user['display_name'];
+        $_SESSION['role'] = $user['role'];
 
+        if ($role == "admin") {
+            header("Location: admin_mainpage.php"); // อย่าลืมทำหน้า admin_mainpage.php
+            exit();
 
+        } else if ($role == "user") {
+            header("Location: main.html"); // ไปหน้า home
+            exit();
 
-
-
-
+        }
+        } else {
+            echo "<script>alert('username or password incorrect!'); window.location='login.html';</script>";
+        }
     }
-
-
 ?>
     
 </body>
