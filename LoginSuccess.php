997b04c8d4fd0e5e862cece0fcfbf780dd464978
pongpak726 <fuza2560@gmail.com>
2025-10-09 21:34:26 +0700

    mod login and LoginSuccess

--- LoginSuccess.php
+++ LoginSuccess.php
@@ -6,6 +6,40 @@
     <title>Login Success</title>
 </head>
 <body>
+
+<?php
+    require 'conn.php';
+
+    if (isset($_POST['login'])) {
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $role = $_POST['role'];
+
+        if ($role == 'admin') {
+            $sql = "SELECT * FROM users 
+            WHERE email='$username' 
+            AND password_hash='$password' 
+            AND role='admin'";
+        } 
+        else {
+            $sql = "SELECT * FROM users 
+            WHERE email='$username' 
+            AND password_hash='$password' 
+            AND role='user'";
+        }   
+
+
+
+
+
+
+
+
+    }
+
+
+?>
     
 </body>
 </html>
